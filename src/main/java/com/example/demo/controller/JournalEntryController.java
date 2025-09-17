c7682ac0fe3fc944bae00ce5e2595accf2cc2714
Soumyodip Sutradhar <soumyodipsutra@gmail.com>
2025-09-17 22:19:02 +0530

    Created REST API

--- src/main/java/com/example/demo/controller/JournalEntryController.java
+++ src/main/java/com/example/demo/controller/JournalEntryController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.JournalEntry;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,11 +9,34 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
 
     private Map<Long, JournalEntry> journalEntries=new HashMap<>();
 
+    @GetMapping
     public List<JournalEntry> getAll(){
         return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping
+    public JournalEntry updateJournalEntryById(@PathVariable long id, @RequestBody JournalEntry myEntry){
+        return journalEntries.put(id,myEntry);
     }
 }
