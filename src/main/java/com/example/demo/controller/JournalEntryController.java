de61e2827c71a2c0d5bddffba82202da7f685008
Soumyodip Sutradhar <soumyodipsutra@gmail.com>
2025-09-17 20:47:04 +0530

    Creating REST API

--- src/main/java/com/example/demo/controller/JournalEntryController.java
+++ src/main/java/com/example/demo/controller/JournalEntryController.java
@@ -1,7 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.JournalEntry;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries=new HashMap<>();
+
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
 }
