0cd44dec9d27daf1eb771dce52cdcf602346cfb0
Soumyodip Sutradhar <soumyodipsutra@gmail.com>
2025-09-20 11:28:40 +0530

    Created REST API

--- src/main/java/com/example/demo/controller/JournalEntryController.java
+++ src/main/java/com/example/demo/controller/JournalEntryController.java
@@ -3,10 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.JournalEntry;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/journal")
